040ca21174f92e607003795973fa3f45a4debdfb
lee-hyunwooo <blacktear7@naver.com>
2023-09-08 12:08:42 +0900

    patter

---
+++ chap14_pattern/src/com/kh/pattern/mvc/UserClient.java
@@ -0,0 +1,22 @@
+package com.kh.pattern.mvc;
+
+import com.kh.pattern.mvc.controller.UserController;
+import com.kh.pattern.mvc.model.UserModel;
+import com.kh.pattern.mvc.view.UserView;
+
+public class UserClient {
+	
+	public static void maint(String[] args) {
+		
+	}
+	/*
+	 MVC 패턴을 사용해서 학생 정보를 다루는곳
+	 */
+	 	UserModel user = new UserModel("홍길동",20);
+	 	
+	 	//뷰 객체 생성
+	 	UserView view = new UserView();
+
+	 	//컨트롤러 객체 생성 및 모델 뷰 설정
+	 	UserController controller = new UserController(user, view);
+}
---
+++ chap14_pattern/src/com/kh/pattern/mvc/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.kh.pattern.mvc.controller;
+
+import com.kh.pattern.mvc.model.UserModel;
+import com.kh.pattern.mvc.view.UserView;
+
+public class UserController {
+   //모델과 뷰를 연결하는 곳
+	//사용자 입력을 처리하는 곳
+
+	private UserModel usermodel;
+	private UserView userview;
+	
+	public UserController(UserModel usermodel, UserView userview) {
+		this.usermodel = usermodel;
+		this.userview = userview;
+	}
+
+	public UserModel getUsermodel() {
+		return usermodel;
+	}
+
+	public void setUsermodel(UserModel usermodel) {
+		this.usermodel = usermodel;
+	}
+
+	public UserView getUserview() {
+		return userview;
+	}
+
+	public void setUserview(UserView userview) {
+		this.userview = userview;
+	}
+
+}
