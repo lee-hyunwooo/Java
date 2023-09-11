562f069698385c956b32c3cf8f832b77e5b73be0
lee-hyunwooo <blacktear7@naver.com>
2023-09-11 11:48:04 +0900

    pattern

--- chap14_pattern/src/com/kh/goodEaxm/game/GameView.java
+++ chap14_pattern/src/com/kh/goodEaxm/game/GameView.java
@@ -1,22 +1,27 @@
 package com.kh.goodEaxm.game;
 
 public class GameView {
-	
+	private String StartMsg = ("숫자 맞추기 게임 시작합니다.");
+	private String StartNum = ("1에서 100 사이의 숫자를 맞춰보세요!");
+	private String Choice = ("추측한 숫자를 입력하세요.");
+	private String Corrcet = ("숫자를 맞췄습니다");
+	private String InCorrcet = ("틀렸습니다. 다시 시도하세요.");
+	private String GameOver = ("게임종료!");
 	public void displayMessage() {
-		System.out.println("숫자 맞추기 게임을 시작합니다.");
-		System.out.println("1에서 100 사이의 숫자를 맞춰보세요!");
+		System.out.println(StartMsg);
+		System.out.println(StartNum);
 	}
 	//숫자를 추측하는 값 입력하고 맞췄는지 틀렸는지 적는거랑 시도횟수 게임종료
 	//1. 숫자 추측하는 값 입력하기
 	public void displayGuessPromt() {
-		System.out.println("추측한 숫자를 입력하세요.");
+		System.out.println(Choice);
 	}
 	
 	public void displayCorrectGuess() {
-		System.out.println("숫자를 맞췄습니다.");
+		System.out.println(Corrcet);
 	}
 	public void displayIncorrectGuess() {
-		System.out.println("틀렸습니다. 다시 시도하세요.");
+		System.out.println(InCorrcet);
 		
 	}
 	
@@ -25,7 +30,7 @@ public class GameView {
 	}
 
 	public void displayGameOver() {
-		System.out.println("게임종료!");
+		System.out.println(GameOver);
 	}
 }
 
