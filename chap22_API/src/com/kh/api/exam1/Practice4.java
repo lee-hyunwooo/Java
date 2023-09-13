5365ddc4a607ace6a34d51a9293a417e439ca854
lee-hyunwooo <blacktear7@naver.com>
2023-09-13 17:05:00 +0900

    API

---
+++ chap22_API/src/com/kh/api/exam1/Practice4.java
@@ -0,0 +1,54 @@
+package com.kh.api.exam1;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Practice4 {
+
+	public static void main(String[] args) {
+		Practice4 pm = new Practice4();
+		pm.practice5();
+	}
+		
+	public void practice1() {
+		//년월일 yyy-MM-dd
+		Date currentDate = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String forDate = sdf.format(currentDate);
+		System.out.println("포멧팅 된 날짜" + forDate);
+	}
+
+	public void practice2() {
+		//시분초 HH:mm:ss
+		Date currentDate = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+		String forDate = sdf.format(currentDate);
+		System.out.println("포멧팅 된 시간" + forDate);
+		
+	}
+	
+	public void practice3() {
+		//월일 MM-dd
+		Date currentDate = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM-dd");
+		String forDate = sdf.format(currentDate);
+		System.out.println("포멧팅 날짜" + forDate);
+		
+	}
+	
+	public void practice4() {
+		//연도-시간 yyyy-HH
+		Date currentDate = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-HH");
+		String forDate = sdf.format(currentDate);
+		System.out.println("포멧팅 연도와 시간" + forDate);
+	}
+	
+	public void practice5() {
+		// 년-월-일 요일 yyyy-MM-dd (E)
+		Date currentDate = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd (E)");
+		String forDate = sdf.format(currentDate);
+		System.out.println("포멧팅 날짜와 요일" + forDate);
+	}
+}
