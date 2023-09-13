d593c7e1b66ab21f2ce89f0b939f9b59405b2add
lee-hyunwooo <blacktear7@naver.com>
2023-09-13 12:06:03 +0900

    API

---
+++ chap22_API/src/com/kh/api/exam1/PracticeMain.java
@@ -0,0 +1,38 @@
+package com.kh.api.exam1;
+
+import java.util.Scanner;
+
+public class PracticeMain {
+
+	public static void main(String[] args) {
+
+		Scanner sc =new Scanner(System.in);
+		System.out.println();
+		
+		
+		
+		//문자열 비교
+		if (str1.equals(str2)) {
+			System.out.println("두 문자열은 같습니다.");
+		} else {
+			System.out.println("두 문자열은 다릅니다.");
+		}
+		
+		//문자열의 해시 코드 출력
+		int hashCode1 = str1.hashCode();
+		int hashCode2 = str2.hashCode();
+		System.out.println("첫 번째 문자열의 해시코드 : " + hashCode1);
+		System.out.println("두 번째 문자열의 해시코드 : " + hashCode2);
+		
+		//문자열의 문자열 표현 출력
+		String str1String = str1.toString();
+		String str2String = str2.toString();
+		System.out.println("첫 번째 문자열의 문자열 표현 : " + str1String);
+		System.out.println("두 번째 문자열의 문자열 표현 : " + str2String);
+		
+		
+		
+		
+	}
+
+}
--- chap22_API/src/com/kh/api/exam1/StringTokenizerMain.java
+++ chap22_API/src/com/kh/api/exam1/StringTokenizerMain.java
@@ -8,6 +8,11 @@ public class StringTokenizerMain {
  						주어진 구분자(기본:공백)를 기준으로 문자열을 토큰으로 분리
  */
 	public static void main(String[] args) {
+		 StringTokenizerMain stm = new StringTokenizerMain();
+		 //stm.STExame();
+		 stm.stExam2();
+		}
+	public void STExam() {
 		String text = "apple orange banana";
 		StringTokenizer tokenizer = new StringTokenizer(text);
 
@@ -16,7 +21,25 @@ public class StringTokenizerMain {
 			String token = tokenizer.nextToken();
 			System.out.println(token);
 		}
-		
 	}
-
+	public void stExam2() {
+		String text = "apple, banana, grape, orange";
+		StringTokenizer tokenizer = new StringTokenizer(text,",");
+		
+		while(tokenizer.hasMoreTokens()) {
+			String fruit = tokenizer.nextToken();
+					System.out.println(fruit);
+		}
+	}
+		public void stExam3() {
+			//- 구분자를 사용해서 text 만들고  - 구분자로 끊어서 출력하기
+			String text1 = "apple-banana-grape-orange";
+			StringTokenizer tokenizer1 = new StringTokenizer(text1,"-");
+			
+			while(tokenizer1.hasMoreTokens()) {
+				String fruit1 = tokenizer1.nextToken();
+					System.out.println(fruit1);
+			}
+		}
+	
 }
