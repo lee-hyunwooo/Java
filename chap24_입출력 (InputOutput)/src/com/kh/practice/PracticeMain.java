44288af5fee9321145516e984a13edd66d52fe0f
lee-hyunwooo <blacktear7@naver.com>
2023-09-14 12:41:33 +0900

    InputOutput

--- chap24_입출력 (InputOutput)/src/com/kh/practice/PracticeMain.java
+++ chap24_입출력 (InputOutput)/src/com/kh/practice/PracticeMain.java
@@ -1,6 +1,8 @@
 package com.kh.practice;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -14,11 +16,19 @@ public class PracticeMain {
 	public void practice1() {//경로설정해서 파일 만들기
 	//	File f = new File
 		try {
-			FileWriter w = new FileWriter("C:\\Users\\user1\\Desktop/ p1.txt");
+			FileWriter w = new FileWriter("C://Users//user1//Desktop// p1.txt");
 			//파일쓰기
-			w.write("경로찾기!");
+			w.write("경로확인");
 			//파일닫기
 			w.close();
+			
+			FileReader reader = new FileReader("C://Users//user1//Desktop// p1.txt");
+			BufferedReader bReader = new BufferedReader(reader);
+			
+			String line;
+			while((line=bReader.readLine()) != null) {
+				System.out.println(line);
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
