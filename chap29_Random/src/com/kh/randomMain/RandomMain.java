dd1f8bd3eec8bd7594fd1167df315156ef69ff8a
lee-hyunwooo <blacktear7@naver.com>
2023-09-18 12:42:08 +0900

    Random

---
+++ chap29_Random/src/com/kh/randomMain/RandomMain.java
@@ -0,0 +1,57 @@
+package com.kh.randomMain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/*
+ Random : 난수를 생성하기 위해 사용되는 클래스
+ 	난수 : 무작위로 만들어진 수
+ */
+public class RandomMain {
+
+	public static void main(String[] args) {
+		// 로또 번호 생성기
+		RandomMain rm = new RandomMain();
+		rm.Lotto();
+		
+	}
+	public void Lotto() {
+		Random rd = new Random();//랜덤 객체 생성
+		//ArratList 사용해서 장바구니로 담기
+		List<Integer> lottoNum = new ArrayList<>();
+		//중복을 피하기 위해 Set을 사용할 수 있음
+		//6 숫자만 들어갈 것
+		int six = 6;//로또 번호 몇개 뽑을 것인지 설정
+		while(lottoNum.size() < six) {//인덱스 6자리
+			//1 ~ 45 범위를 지정해서 난수 생성
+			int ranNumber = rd.nextInt(45)+1; //0~44자리의 랜덤으로 번호 하나
+			lottoNum.add(ranNumber);
+			
+			//중복된 값이 list에 저장될 수 있음
+			if(!lottoNum.contains(ranNumber)) {//!가 사라지면 값이 존재할 때만 list에 저장되는 것이기 때문에 무한루프
+				lottoNum.add(ranNumber);
+			}
+		}
+		System.out.println("로또 번호 " + lottoNum);
+		
+		
+		
+	}
+	
+	public void RandomExam() {
+		Random rd = new Random();//랜덤 객체 생성
+		
+		//1. 정수 범위 내에서 랜덤인 난수를 만들어야겠다.
+		int ranInt = rd.nextInt(100);//0~99 사이에 1개
+		System.out.println(ranInt);
+		
+		//2. 실수 범위 내에서 랜덤인 난수를 만들어야겠다.
+		double ranD = rd.nextDouble();
+		System.out.println(ranD);
+		
+		//3. boolean 값 랜더므로 생성
+		boolean ranB = rd.nextBoolean();
+		System.out.println(ranB);
+	}
+}
