53c620ebbc5a4199fd882958ffb97a82be1b5e93
lee-hyunwooo <blacktear7@naver.com>
2023-09-19 09:49:48 +0900

    Thread

--- chap30_Thread/src/com/kh/ThreadPractice/PracticeMain.java
+++ chap30_Thread/src/com/kh/ThreadPractice/PracticeMain.java
@@ -5,8 +5,11 @@ import com.kh.ThreadPractice.GameLoop1;
 
 public class PracticeMain extends GameLoop1{
 	private static boolean isGameOver = false;
+	
 	public static void main(String[] args) {
+	
 		PracticeMain p = new PracticeMain();
+		
 		Thread gameThread = new Thread(new GameLoop1());
 		gameThread.start();
 		
---
+++ chap30_Thread/src/com/kh/result/getterSetter/GameLoop.java
@@ -0,0 +1,26 @@
+package com.kh.result.getterSetter;
+
+public class GameLoop implements Runnable {
+	 private static boolean isGameOver = false;
+	    
+	public static void setGameOver(boolean isGameOver) {
+		GameLoop.isGameOver = isGameOver;
+	}
+	@Override
+   public void run() {
+       //boolean isGameOver = false;
+       while (!isGameOver) {
+           // 게임 루프 내용
+           int randomValue = new java.util.Random().nextInt(10); // 임의의 숫자 생성
+           System.out.println("임의의 숫자: " + randomValue);
+
+           // 게임 루프 지연 시간
+           try {
+               Thread.sleep(1000); // 1초 대기
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+       }
+       System.out.println("게임 종료");
+   }
+}
