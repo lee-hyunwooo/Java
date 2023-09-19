1ee21edee9e2a66fc18be027714663f6c36c87e7
lee-hyunwooo <blacktear7@naver.com>
2023-09-19 10:13:36 +0900

    Tree

--- chap31_Tree/src/com/kh/treeMain/BinaryTree.java
+++ chap31_Tree/src/com/kh/treeMain/BinaryTree.java
@@ -11,11 +11,15 @@ public class BinaryTree {
 	public BinaryTree() {
 		this.root = null;
 	}
+	//insert 메서드와 insertRac 메서드는 이진 트리에 새로운 노드를 삽입하는데 사용
 	
+	//insert : 외부에 노출되는 메서드
 	public void insert(int data) {
 		root = insertRec(root, data);
 	}
-	
+	//insertRec : 실제 삽입 작업 메서드
+	//재귀적 : 자기 자신을 포함하고 다시 자기 자신을 사용해서 정의 내림
+	//재귀적으로 노드를 삽입하고 적절한 위치를 찾아 새로운 노드를 생성
 	private TreeNode insertRec(TreeNode root, int data) {
 		if(root == null) {
 			root = new TreeNode(data);
@@ -27,6 +31,22 @@ public class BinaryTree {
 			root.right = insertRec(root.right,data);
 		}
 			return root;
+	}
+	/*
+	 inOrderT : 중위순회
+	 inOrderTR : 중위 순회를 수행하여 트리의 노드를 출력하는데 사용
+	 왼쪽 서브트리 현재노드 오른쪽 서브트리 순서로 노드 출력
+	 */
+	public void onOrderT() {//inOrder Traversal 중위 순회
+		inOrderTR(root);
+	}
+	
+	private void inOrderTR(TreeNode root) {
+		if (root != null) {
+			inOrderTR(root.left);
+			System.out.println(root.data + " ");
+			inOrderTR(root.right);
+		}
 	}
 	public static void main(String[] args) {
 		
